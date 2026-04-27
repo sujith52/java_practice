2edc275556aa374a95f45f00ed1f6151c1b2277b
sujith kumar <148097955+sujith52@users.noreply.github.com>
2026-04-27 15:01:30 +0530

    Add files via upload

---
+++ 26_march/pracv17.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class pracv17 {
+    public static void main(String[] args) {
+        long binary1, binary2;
+        int i = 0, remainder = 0;
+
+        int [] sum  = new int[20];
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the binary num1 :");
+        binary1 = sc.nextLong();
+        System.out.println("Enter the 2nd binry num :");
+        binary2 = sc.nextLong();
+
+        while(binary1 !=0 || binary2 != 0)
+        {
+            sum[i++] = (int)(())
+        }
+    }
+}
