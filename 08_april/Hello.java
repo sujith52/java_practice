40d94b5fbc3f5c22db483c1b8d1fc9c707346fba
sujith kumar <148097955+sujith52@users.noreply.github.com>
2026-05-08 21:53:42 +0530

    Create Hello.java

---
+++ 08_april/Hello.java
@@ -0,0 +1,33 @@
+public class Hello{
+    public static void main(String[] args) {
+        System.err.println("This was the world sreeja and latha !");
+        int na = 556;
+        byte mb = (byte) na;
+        System.err.println(mb);
+
+        int num1 = 6;
+        num1 += 2;
+        num1++;
+        --num1;
+        // int num1 = 8;
+        int results = ++num1;
+        System.err.println(num1);
+
+        // bolean values 
+
+        int x = 5;
+        int y = 8;
+
+        boolean result = x < y;
+        System.err.println(result);
+
+        int x = 34;
+        int y = 67;
+
+        int a = 5;
+        int b = 6;
+        if (x < y && a < b) {
+            System.err.println("This is the good morning bros !");
+        }
+    }
+}
